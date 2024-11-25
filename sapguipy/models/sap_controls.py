2701d3044ae0a01464ae4b16158ecc5db26517b7
Nicolas Passos <nicolasduart21@gmail.com>
2024-11-25 14:57:29 -0400

    Separação de classes modelos da classe SapGui

---
+++ sapguipy/models/sap_controls.py
@@ -0,0 +1,382 @@
+from pandas import DataFrame
+
+class GuiVComponent:
+    def __init__(self, element):
+        self.element = element
+
+    @property
+    def id(self):
+        return self.element.Id
+    
+    @property
+    def name(self):
+        return self.element.Name
+    @property
+    def changeable(self):
+        return self.element.Changeable
+    
+    @property
+    def default_tooltip(self):
+        return self.element.DefaultTooltip if self.element.DefaultTooltip != '' else None
+    
+    @property
+    def height(self):
+        return self.element.Height
+    
+    @property
+    def icon_name(self):
+        return self.element.IconName
+    
+    @property
+    def left(self):
+        return self.element.Left
+    
+    @property
+    def modified(self):
+        return self.element.Modified if self.element.ContainerType else None
+    
+    @property
+    def screen_left(self):
+        return self.element.ScreenLeft
+    
+    @property
+    def screen_top(self):
+        return self.element.ScreenTop
+    
+    @property
+    def text(self):
+        return self.element.Text if self.element.Text != '' else None
+    
+    @property
+    def tooltip(self):
+        return self.element.Tooltip if self.element.Tooltip != '' else None
+    
+    @property
+    def icon_name(self):
+        return self.element.IconName if self.element.IconName != '' else None
+    
+    @property
+    def top(self):
+        return self.element.Top
+
+class GuiButton(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def press(self):
+        """Presses the button."""
+        self.element.Press()
+
+class GuiTextField(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def set_text(self, text):
+        """Sets the text of the text field."""
+        self.element.Text = text
+    
+    def get_text(self):
+        """Returns the text of the text field."""
+        return self.element.Text
+
+class GuiComboBox(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def select_entry(self, entry):
+        """Selects an entry in the combo box."""
+        self.element.Key = entry
+
+class GuiCheckBox(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+
+    @property
+    def selected(self):
+        return self.element.Selected
+    
+    def select(self, value=True):
+        """Sets the value of the CheckBox."""
+        self.element.Selected = value
+
+class GuiCTextField(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def set_text(self, text):
+        """Sets the text of the CTextField."""
+        self.element.Text = text
+    
+    def get_text(self):
+        """Returns the text of the CTextField."""
+        return self.element.Text if self.element.Text != '' else None
+
+class GuiTab(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def select(self):
+        """Selects the tab."""
+        self.element.Select()
+
+class GuiGridView(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def select_row(self, row):
+        """Selects a row in the grid view."""
+        self.element.Rows.SelectedRow = row
+    
+    def get_cell_value(self, row, column):
+        """Returns the value of a specific cell."""
+        return self.element.GetCellValue(row, column)
+    
+    def set_cell_value(self, row, column, value):
+        """Defines the value of a specific cell."""
+        self.element.SetCellValue(row, column, value)
+
+class GuiShell(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+
+    @property
+    def rows_count(self):
+        return self.element.RowCount
+    
+    @property
+    def columns_order(self):
+        return [item for item in self.element.ColumnOrder]
+    
+    @property
+    def drag_drop_supported(self):
+        return self.element.DragDropSupported
+    
+    def send_command(self, command):
+        """Sends a command to the shell."""
+        self.element.SendCommand(command)
+    
+    def get_cell_value(self, row, column):
+        """Returns the value of a specific cell."""
+        return self.element.GetCellValue(row, column)
+
+    def read_shell_table(self) -> DataFrame:
+        """Return a shell table as a pandas DataFrame."""
+        columns = self.columns_order
+        rows_count = self.rows_count
+        data = [
+                {column: self.get_cell_value(i, column) for column in columns}
+                for i in range(rows_count)
+                ]
+        
+        return DataFrame(data)
+
+class GuiTree(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def expand_node(self):
+        """Expands a node in the tree."""
+        self.element.Expand()
+    
+    def collapse_node(self):
+        """Collapses a node in the tree."""
+        self.element.Collapse()
+    
+    def select_node(self, node_key):
+        """Selects a node in the tree."""
+        self.element.SelectNode(node_key)
+
+class GuiStatusbar(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    @property
+    def message_type(self):
+        match self.element.MessageType:
+            case "S":
+                return "Success"
+            case "E":
+                return "Error"
+            case "W":
+                return "Warning"
+            case "I":
+                return "Information"
+            case "A":
+                return "Abort"
+            case _:
+                return None
+    
+    @property
+    def has_popup(self):
+        return self.element.MessageAsPopup
+    
+    def get_text(self):
+        """Returns the text of the status bar."""
+        return self.element.Text if self.element.Text != '' else None
+
+class GuiFrameWindow(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def maximize(self):
+        """Maximizes the window."""
+        self.element.Maximize()
+    
+    def minimize(self):
+        """Minimizes the window."""
+        self.element.Iconify()
+    
+    def restore(self):
+        """Restore the original window size."""
+        self.element.Restore()
+
+    def close_session(self):
+        """Ends the current session."""
+        self.session.EndSession()
+
+class GuiSessionInfo(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def get_user(self):
+        """Returns the user of the session."""
+        return self.element.User
+    
+    def get_client(self):
+        """Returns the client of the session."""
+        return self.element.Client
+    
+    def get_transaction(self):
+        """Returns the transaction of the session."""
+        return self.element.Transaction
+    
+    def get_program(self):
+        """Returns the program of the session."""
+        return self.element.Program
+    
+    def get_system(self):
+        """Returns the system of the session."""
+        return self.element.System
+    
+class GuiApplication(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    @property
+    def name(self):
+        """Returns the name of the application."""
+        return self.element.Name
+    
+    @property
+    def version(self):
+        """Returns the version of the application."""
+        return self.element.Version
+    
+    def open_connection(self, connection_string):
+        """Returns the name of the application."""
+        return self.element.OpenConnection(connection_string)
+    
+class GuiLabel(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    @property
+    def text(self):
+        """Gets the text of the label."""
+        return self.element.Text
+    
+    def get_text(self):
+        """Returns the text of the label."""
+        return self.element.Text if self.element.Text != '' else None
+    
+    def set_text(self, text):
+        """Sets the text of the label."""
+        self.element.Text = text
+
+class GuiToolbar(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def press_button(self, button_id):
+        """Presses a button in the toolbar."""
+        self.element.PressButton(button_id)
+
+class GuiTableControl(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def set_cell_value(self, row, column, value):
+        """Sets the value of a specific cell."""
+        self.element.SetCellValue(row, column, value)
+    
+    def get_cell_value(self, row, column):
+        """Returns the value of a specific cell."""
+        return self.element.GetCellValue(row, column)
+
+class GuiTitlebar(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    @property
+    def text(self):
+        """Gets the text of the title bar."""
+        return self.element.Text
+
+class GuiContainer(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def find_by_name(self, name):
+        """Finds a control by name."""
+        return self.element.FindByName(name)
+
+class GuiSplitter(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    @property
+    def element(self):
+        return self.element
+
+class GuiUserArea(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    def list_children(self):
+        """List all children of the user area."""
+        return [item for item in self.element.Children]
+    
+class GuiMainWindow(GuiVComponent):
+    def __init__(self, element):
+        self.element = element
+    
+    @property
+    def v_key_allowed(self):
+        return self.element.IsVKeyAllowed
+
+    def maximize(self):
+        """Maximizes the window."""
+        self.element.Maximize()
+    
+    def minimize(self):
+        """Minimizes the window."""
+        self.element.Iconify()
+    
+    def restore(self):
+        """Restore the original window size."""
+        self.element.Restore()
+
+    def send_v_key(self, key):
+        """Sends a VKey to the window."""
+        self.element.SendVKey(key)
+
+    def show_message_box(self):
+        self.element.ShowMessageBox()
+
+    def tab_backward(self):
+        self.element.TabBackward()
+
+    def tab_forward(self):
+        self.element.TabForward()
+    
+    def close(self):
+        self.element.Close()
--- sapguipy/sap.py
+++ sapguipy/sap.py
@@ -3,7 +3,6 @@ from pythoncom import CoInitialize
 from pywinauto import Application
 from subprocess import Popen
 from threading import Thread
-from pandas import DataFrame
 from platform import system
 from random import randint
 from pathlib import Path
@@ -12,6 +11,7 @@ from time import sleep
 import win32com.client
 import psutil
 from sapguipy.models.exceptions import ElementNotFound
+from sapguipy.models.sap_controls import *
 
 class SapGui:
     def __init__(self, sid: str, user: str, pwd: str, mandante: str, root_sap_dir: str='C:\Program Files (x86)\SAP\FrontEnd\SAPGUI'):
@@ -49,7 +49,7 @@ class SapGui:
         This method attempts to launch the SAP application on a Windows OS and 
         establish a session by logging in with the provided credentials. It handles 
         multiple login attempts and manages password changes if required. The method 
-        sets the `logado` attribute to `True` upon successful login.
+        sets the `logged` attribute to `True` upon successful login.
 
         Raises:
             Exception: If the SAP application fails to start after multiple attempts 
@@ -97,21 +97,27 @@ class SapGui:
             
             if self.find_by_id("wnd[1]/usr/lblRSYST-NCODE_TEXT", False):
                 # Se aparecer a tela de troca de senha, resetar a senha
-                _current_date = datetime.now()
-                _random = randint(0,100)
-                _date = _current_date + timedelta(days=_random)
-                self.new_pwd = _date.strftime("%B@%Y%H%M%f")
-
-                self.find_by_id("wnd[1]/usr/pwdRSYST-NCODE").set_text(self.new_pwd)
-                self.find_by_id("wnd[1]/usr/pwdRSYST-NCOD2").set_text(self.new_pwd)
-                self.find_by_id("wnd[1]/tbar[0]/btn[0]").press()
-
-                if self.find_by_id("wnd[1]/usr/txtMESSTXT1"):
-                    self.find_by_id("wnd[1]/tbar[0]/btn[0]").press()
-
+                self.change_password()
             self.logged = True
         else:
             raise Exception('This library only supports Windows OS')
+        
+    def change_password(self):
+        """
+        If the password is expired, SAP will open an modal to change the password.
+        This method changes the password in this modal.
+        """
+        _current_date = datetime.now()
+        _random = randint(0,100)
+        _date = _current_date + timedelta(days=_random)
+        self.new_pwd = _date.strftime("%B@%Y%H%M%f")
+
+        self.find_by_id("wnd[1]/usr/pwdRSYST-NCODE").set_text(self.new_pwd)
+        self.find_by_id("wnd[1]/usr/pwdRSYST-NCOD2").set_text(self.new_pwd)
+        self.find_by_id("wnd[1]/tbar[0]/btn[0]").press()
+
+        if self.find_by_id("wnd[1]/usr/txtMESSTXT1", False):
+            self.find_by_id("wnd[1]/tbar[0]/btn[0]").press()
         
     def get_user_logged(self):
         
@@ -184,16 +190,11 @@ class SapGui:
         self.logged = False
 
     def open_transaction(self,transacao: str):
-        sleep(1)
+        """Open an SAP transaction."""
         self.session.startTransaction(transacao)
     
     def get_window_size(self):
-        try:
-            height = self.find_by_id("wnd[0]").Height
-            width = self.find_by_id("wnd[0]").Width
-            return width, height
-        except Exception:
-            raise Exception('Não foi possível obter o tamanho da janela do SAP')
+        return self.find_by_id("wnd[0]").width, self.find_by_id("wnd[0]").height
         
     def verify_sap_connection(self):
         while self.logged:
@@ -263,280 +264,9 @@ class SapGui:
                 return GuiContainer(element)
             case "GuiSplitter":
                 return GuiSplitter(element)
+            case "GuiUserArea":
+                return GuiUserArea(element)
+            case "GuiMainWindow":
+                return GuiMainWindow(element)
             case _:
                 raise TypeError(f"Element type '{element_type}' is not supported.")
-                
-
-class GuiButton:
-    def __init__(self, element):
-        self.element = element
-    
-    def press(self):
-        """Presses the button."""
-        self.element.Press()
-
-class GuiTextField:
-    def __init__(self, element):
-        self.element = element
-    
-    def set_text(self, text):
-        """Sets the text of the text field."""
-        self.element.Text = text
-    
-    def get_text(self):
-        """Returns the text of the text field."""
-        return self.element.Text
-
-class GuiComboBox:
-    def __init__(self, element):
-        self.element = element
-    
-    def select_entry(self, entry):
-        """Selects an entry in the combo box."""
-        self.element.Key = entry
-
-class GuiCheckBox:
-    def __init__(self, element):
-        self.element = element
-    
-    def select(self, value=True):
-        """Sets the value of the CheckBox."""
-        self.element.Selected = value
-
-class GuiCTextField:
-    def __init__(self, element):
-        self.element = element
-    
-    def set_text(self, text):
-        """Sets the text of the CTextField."""
-        self.element.Text = text
-    
-    def get_text(self):
-        """Returns the text of the CTextField."""
-        return self.element.Text if self.element.Text != '' else None
-
-class GuiTab:
-    def __init__(self, element):
-        self.element = element
-    
-    def select(self):
-        """Selects the tab."""
-        self.element.Select()
-
-class GuiGridView:
-    def __init__(self, element):
-        self.element = element
-    
-    def select_row(self, row):
-        """Selects a row in the grid view."""
-        self.element.Rows.SelectedRow = row
-    
-    def get_cell_value(self, row, column):
-        """Returns the value of a specific cell."""
-        return self.element.GetCellValue(row, column)
-    
-    def set_cell_value(self, row, column, value):
-        """Defines the value of a specific cell."""
-        self.element.SetCellValue(row, column, value)
-
-class GuiShell:
-    def __init__(self, element):
-        self.element = element
-
-    @property
-    def rows_count(self):
-        return self.element.RowCount
-    
-    @property
-    def columns_order(self):
-        return [item for item in self.element.ColumnOrder]
-    
-    def send_command(self, command):
-        """Sends a command to the shell."""
-        self.element.SendCommand(command)
-    
-    def get_cell_value(self, row, column):
-        """Returns the value of a specific cell."""
-        return self.element.GetCellValue(row, column)
-
-    def read_shell_table(self) -> DataFrame:
-        """Return a shell table as a pandas DataFrame."""
-        columns = self.columns_order
-        rows_count = self.rows_count
-        data = [
-                {column: self.get_cell_value(i, column) for column in columns}
-                for i in range(rows_count)
-                ]
-        
-        return DataFrame(data)
-
-class GuiTree:
-    def __init__(self, element):
-        self.element = element
-    
-    def expand_node(self):
-        """Expands a node in the tree."""
-        self.element.Expand()
-    
-    def collapse_node(self):
-        """Collapses a node in the tree."""
-        self.element.Collapse()
-    
-    def select_node(self, node_key):
-        """Selects a node in the tree."""
-        self.element.SelectNode(node_key)
-
-class GuiStatusbar:
-    def __init__(self, element):
-        self.element = element
-    
-    def get_text(self):
-        """Returns the text of the status bar."""
-        return self.element.Text if self.element.Text != '' else None
-
-class GuiFrameWindow:
-    def __init__(self, element):
-        self.element = element
-    
-    def maximize(self):
-        """Maximizes the window."""
-        self.element.Maximize()
-    
-    def minimize(self):
-        """Minimizes the window."""
-        self.element.Iconify()
-    
-    def restore(self):
-        """Restore the original window size."""
-        self.element.Restore()
-
-    def close_session(self):
-        """Ends the current session."""
-        self.session.EndSession()
-
-class GuiSession(SapGui):
-    def __init__(self, element):
-        self.element = element
-
-    @property
-    def info(self):
-        return GuiSessionInfo(self.element)
-    
-    def send_vkey(self, vkey):
-        """Sends a virtual key to the session."""
-        self.element.SendVKey(vkey)
-    
-    def end_session(self):
-        """Ends the current session."""
-        self.element.EndSession()
-    
-    def start_transaction(self, transaction):
-        """Starts a new transaction."""
-        self.element.StartTransaction(transaction)
-    
-    def close_transaction(self):
-        """Ends the current transaction."""
-        self.element.EndTransaction()
-
-class GuiSessionInfo:
-    def __init__(self, element):
-        self.element = element
-    
-    def get_user(self):
-        """Returns the user of the session."""
-        return self.element.User
-    
-    def get_client(self):
-        """Returns the client of the session."""
-        return self.element.Client
-    
-    def get_transaction(self):
-        """Returns the transaction of the session."""
-        return self.element.Transaction
-    
-    def get_program(self):
-        """Returns the program of the session."""
-        return self.element.Program
-    
-    def get_system(self):
-        """Returns the system of the session."""
-        return self.element.System
-    
-class GuiApplication:
-    def __init__(self, element):
-        self.element = element
-    
-    @property
-    def name(self):
-        """Returns the name of the application."""
-        return self.element.Name
-    
-    @property
-    def version(self):
-        """Returns the version of the application."""
-        return self.element.Version
-    
-    def open_connection(self, connection_string):
-        """Returns the name of the application."""
-        return self.element.OpenConnection(connection_string)
-    
-class GuiLabel:
-    def __init__(self, element):
-        self.element = element
-    @property
-    def text(self):
-        """Gets the text of the label."""
-        return self.element.Text
-    
-    def get_text(self):
-        """Returns the text of the label."""
-        return self.element.Text if self.element.Text != '' else None
-    
-    def set_text(self, text):
-        """Sets the text of the label."""
-        self.element.Text = text
-
-class GuiToolbar:
-    def __init__(self, element):
-        self.element = element
-    
-    def press_button(self, button_id):
-        """Pressiona um botão na barra de ferramentas."""
-        self.element.PressButton(button_id)
-
-class GuiTableControl:
-    def __init__(self, element):
-        self.element = element
-    
-    def set_cell_value(self, row, column, value):
-        """Define o valor de uma célula específica no controle de tabela."""
-        self.element.SetCellValue(row, column, value)
-    
-    def get_cell_value(self, row, column):
-        """Obtém o valor de uma célula específica no controle de tabela."""
-        return self.element.GetCellValue(row, column)
-
-class GuiTitlebar:
-    def __init__(self, element):
-        self.element = element
-    
-    @property
-    def text(self):
-        """Obtém o texto da barra de título."""
-        return self.element.Text
-
-class GuiContainer:
-    def __init__(self, element):
-        self.element = element
-    
-    def find_by_name(self, name):
-        """Encontra um elemento dentro do container pelo nome."""
-        return self.element.FindByName(name)
-
-class GuiSplitter:
-    def __init__(self, element):
-        self.element = element
-    
-    def set_position(self, position):
-        """Define a posição do divisor (splitter)."""
-        self.element.SetPosition(position)
